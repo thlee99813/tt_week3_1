d5d4ea90bd422ea0a7c9b27029b7f8dfe2c2aed3
thlee99813@gmail.com <thlee99813@gmail.com>
2026-04-01 11:03:20 +0900

    [이태환] 테스트씬 및 카메라 디버거 설치 완료

--- Assets/01.Scripts/DebugCamera.cs
+++ Assets/01.Scripts/DebugCamera.cs
@@ -1,14 +1,64 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using Unity.Cinemachine;
 public class DebugCamera : MonoBehaviour
 {
-    void Start()
+    public CinemachineCamera[] cams;
+    public int activePriority = 20;
+    public int inactivePriority = 0;
+
+    public int CurrentCameraIndex { get; private set; }
+
+
+    private void Start()
     {
-        
+        ActivateCamera(0);
+
+    }
+    private void Update()
+    {
+
+        if (Keyboard.current.digit0Key.wasPressedThisFrame)
+        {
+            ActivateCamera(0);
+
+        }
+        else if (Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            ActivateCamera(1);
+
+        }
+        else if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            ActivateCamera(2);
+
+        }
+        else if (Keyboard.current.digit3Key.wasPressedThisFrame)
+        {
+            ActivateCamera(3);
+
+        }
+        else if (Keyboard.current.digit4Key.wasPressedThisFrame)
+        {
+            ActivateCamera(4);
+
+        }
+   
+        else if (Keyboard.current.digit5Key.wasPressedThisFrame)
+        {
+            ActivateCamera(5);
+        }
+    }
+    public void ActivateCamera(int index)
+    {
+        CurrentCameraIndex = index;
+
+        for (int i = 0; i < cams.Length; i++)
+        {
+            cams[i].Priority = (i == index) ? activePriority : inactivePriority;
+        }
     }
 
-    void Update()
-    {
-        
-    }
+
+
 }
